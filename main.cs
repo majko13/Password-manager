07f3bcf8c4fd5924f809828b040af7484efd76ef
majko13 <miklovicm.07@spst.eu>
2025-10-16 00:21:08 +0200

    Prihlasovani do applikace

--- main.cs
+++ main.cs
@@ -28,7 +28,76 @@ namespace Password_manager
 
         private void button1_Click(object sender, EventArgs e)
         {
-        
+            string user = textBox1.Text;
+            string pass = textBox2.Text;
+
+            try
+            {
+                conn.Open();
+                string query = String.Format("SELECT username FROM users WHERE username = '{0}'", user);
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string username = reader["username"].ToString();
+                    reader.Close();
+                    query = String.Format("SELECT id, password FROM users WHERE username = '{0}'", user);
+
+
+                    MySqlCommand cmd2 = new MySqlCommand(query, conn);
+                    reader = cmd2.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        string hash = reader["password"].ToString();
+
+
+
+                        if (BCrypt.Net.BCrypt.EnhancedVerify(pass, hash))
+                        {
+                            string id = reader["id"].ToString();
+
+                            reader.Close();
+
+
+
+
+                            Form credentials = new credentials();
+                            credentials.Show();
+
+                            this.Hide();
+                            
+                        }
+
+                        else
+                        {
+                            reader.Close();
+                            throw new Exception("Zadli jste špatné heslo.");
+                        }
+                    }
+                }
+
+                else
+                {
+                    throw new Exception("Nenašel se učet se stejným jménem.");
+                }
+            }
+
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("login error " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            finally
+            {
+                conn.Close();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
